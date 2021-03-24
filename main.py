6f7cd9609e287cc30f4bf5cc4c1d3873a81f002b
Demirjoe <can.erkiralp@gmail.com>
2021-03-24 17:18:15 +0300

    Color editing

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def on_enterF(e):
 
 
 def on_leaveF(e):
-    FileB['background'] = light2
+    FileB['background'] = dark2
 
 
 def on_enterD(e):
@@ -32,7 +32,7 @@ def on_enterD(e):
 
 
 def on_leaveD(e):
-    Del['background'] = light2
+    Del['background'] = dark2
 
 
 def on_enterN(e):
@@ -40,11 +40,11 @@ def on_enterN(e):
 
 
 def on_leaveN(e):
-    NB['background'] = light2
+    NB['background'] = dark2
 
 
-Books = Listbox(root, font=("Arial", 12, "bold"), bg="light gray", bd=0, height=15, width=80, highlightcolor=med)
-Books.place(x=70, y=70)
+Books = Listbox(root, font=("Arial", 12, "bold"), bg="light gray", bd=0, height=15, width=80, selectbackground=light2)
+Books.place(x=70, y=90)
 
 books = []
 
@@ -56,17 +56,17 @@ for entry in os.listdir(base):
 populate(books)
 print(books)
 
-FileB = Button(root, font=("Arial", 12), text='Files', highlightthickness=0, bg=light2, fg='white', borderwidth=0, width=15, height=2)
+FileB = Button(root, font=("Arial", 12), text='Files', highlightthickness=0, bg=dark2, fg='white', borderwidth=0, width=15, height=2)
 FileB.place(x=0, y=0)
 FileB.bind("<Enter>", on_enterF)
 FileB.bind("<Leave>", on_leaveF)
 
-Del = Button(root, font=("Arial", 12), text='Delete', highlightthickness=0, bg=light2, fg='white', borderwidth=0, width=15, height=2)
+Del = Button(root, font=("Arial", 12), text='Delete', highlightthickness=0, bg=dark2, fg='white', borderwidth=0, width=15, height=2)
 Del.place(x=140, y=0)
 Del.bind("<Enter>", on_enterD)
 Del.bind("<Leave>", on_leaveD)
 
-NB = Button(root, font=("Arial", 12), text='New Book', highlightthickness=0, bg=light2, fg='white', borderwidth=0, width=15, height=2)
+NB = Button(root, font=("Arial", 12), text='New Book', highlightthickness=0, bg=dark2, fg='white', borderwidth=0, width=15, height=2)
 NB.place(x=280, y=0)
 NB.bind("<Enter>", on_enterN)
 NB.bind("<Leave>", on_leaveN)
@@ -77,4 +77,6 @@ Can1.place(x=420, y=0)
 Logo = Label(root, font=("Arial", 18, "bold"), text="FlyPyLog", bg=light2, fg=dark2)
 Logo.place(x=800, y=5)
 
+lBoxLabel = Label(root, font=("Arial", 12, "bold"), text="My Books", bg="white", fg=dark2)
+lBoxLabel.place(x=75, y=60)
 root.mainloop()
