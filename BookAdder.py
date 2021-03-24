c8981cdbad2cb504d039a5f60f37a20bc8cf6f87
Demirjoe <can.erkiralp@gmail.com>
2021-03-24 18:43:46 +0300

    Deleting Ability

---
+++ BookAdder.py
@@ -0,0 +1,2 @@
+# Imports
+from tkinter import *
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Imports
 from tkinter import *
+from tkinter import messagebox
 import os
+import shutil
 
 # Window Setup
 dark2 = "#103B82"
@@ -15,9 +17,29 @@ root.geometry("1000x500")
 root.configure(bg="white")
 root.resizable(False, False)
 
-def populate(lst):
-    Books.insert("end", lst)
+books = []
 
+def Delete():
+    selecind = Books.curselection()
+    fselec = list(selecind)[0]
+    print(selecind)
+    delyn = messagebox.askyesno(title="Delete", message="Do you wish to proceed with the deletion?")
+    print(delyn)
+    if delyn == True:
+        toDel = Books.get(selecind)
+        print(toDel)
+        delet = str(os.path.abspath(os.getcwd())) + "/Books/" + str(toDel[0])
+        shutil.rmtree(delet)
+        Books.delete(fselec, last=None)
+    lBoxer()
+
+def lBoxer():
+    books.clear()
+    base = str(os.path.abspath(os.getcwd())) + "/Books"
+    for entry in os.listdir(base):
+        if os.path.isdir(os.path.join(base, entry)):
+            books.append(entry)
+    Books.insert("end", books)
 
 def on_enterF(e):
     FileB['background'] = med
@@ -46,22 +68,14 @@ def on_leaveN(e):
 Books = Listbox(root, font=("Arial", 12, "bold"), bg="light gray", bd=0, height=15, width=80, selectbackground=light2)
 Books.place(x=70, y=90)
 
-books = []
-
-base = str(os.path.abspath(os.getcwd())) + "/Books"
-for entry in os.listdir(base):
-    if os.path.isdir(os.path.join(base, entry)):
-        books.append(entry)
-
-populate(books)
-print(books)
+lBoxer()
 
 FileB = Button(root, font=("Arial", 12), text='Files', highlightthickness=0, bg=dark2, fg='white', borderwidth=0, width=15, height=2)
 FileB.place(x=0, y=0)
 FileB.bind("<Enter>", on_enterF)
 FileB.bind("<Leave>", on_leaveF)
 
-Del = Button(root, font=("Arial", 12), text='Delete', highlightthickness=0, bg=dark2, fg='white', borderwidth=0, width=15, height=2)
+Del = Button(root, font=("Arial", 12), text='Delete', highlightthickness=0, bg=dark2, fg='white', borderwidth=0, width=15, height=2, command=Delete)
 Del.place(x=140, y=0)
 Del.bind("<Enter>", on_enterD)
 Del.bind("<Leave>", on_leaveD)
